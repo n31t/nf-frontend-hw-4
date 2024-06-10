2817c61eb838adf88dd4438d76bf2ea1699cae5f
Amir Adilov <amiradilov202@gmail.com>
2024-06-11 01:20:06 +0600

    refactor: Update product page to display product details

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import { Product } from "@/app/components/types/product";
 
-const ProductPage = ({ product } : { product : Product}) => {
+const ProductPage = ({ params: { id } }: { params: { id: number } }) => {
     return (
         <div>
             <h1>We don't need it in this homework</h1>
-            <h1>{product.title}</h1>
-            <p>{product.description}</p>
-            <p>{product.price}</p>
-            <p>{product.category}</p>
-            <img src={product.image} alt={product.title} />
-
+            <p>But here is the id: {id}</p>
         </div>
     )
 }
