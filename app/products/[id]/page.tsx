93a33c9fd2e28d62e3a78c37b381efde18c8575b
Amir Adilov <amiradilov202@gmail.com>
2024-06-11 01:21:11 +0600

    refactor: Add header and footer components to product page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Header from "@/app/components/layout/header";
+import Footer from "@/app/components/layout/footer";
 
 const ProductPage = ({ params: { id } }: { params: { id: number } }) => {
     return (
         <div>
+            <Header />
+            <div className=" h-4/5">
             <h1>We don't need it in this homework</h1>
             <p>But here is the id: {id}</p>
+            </div>
+            <Footer />
         </div>
     )
 }
