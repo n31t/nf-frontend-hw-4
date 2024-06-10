eb7cad338e5924a97efd5038f79e118730f3d941
Amir Adilov <amiradilov202@gmail.com>
2024-06-11 01:18:33 +0600

    refactor: Update product page to display product details

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -4,7 +4,13 @@ import { Product } from "@/app/components/types/product";
 const ProductPage = ({ product } : { product : Product}) => {
     return (
         <div>
-            
+            <h1>We don't need it in this homework</h1>
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+            <p>{product.price}</p>
+            <p>{product.category}</p>
+            <img src={product.image} alt={product.title} />
+
         </div>
     )
 }
